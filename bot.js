27a40f4b8fe27e7330fdd335016312b268ce128b
Thuan Huynh <tbh5016@gmail.com>
2017-06-04 23:48:59 -0400

    Learning How to Use Node - Twitter Bot

---
+++ bot.js
@@ -0,0 +1,10 @@
+console.log("the bot is runnning");
+
+var Twit =require('twit');
+
+var T = new Twit({
+  consumer_key:         '...',
+  consumer_secret:      '...',
+  access_token:         '...',
+  access_token_secret:  '...'
+})
